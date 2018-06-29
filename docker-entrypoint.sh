7eabc835ccf93482d3aa6c4923ab9b2564d396de
MetalArend <metalarend@gmail.com>
2018-06-30 00:11:53 +0200

    add phpcs and phpcbf too

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,14 @@ elif [ "$1" = "/composer/vendor/bin/php-compatibility" ]; then
   set -- /sbin/tini -- php "$@"
 elif [ "$1" = "php-compatibility" ]; then
   set -- /sbin/tini -- php /composer/vendor/bin/"$@"
+elif [ "$1" = "/composer/vendor/bin/phpcs" ]; then
+  set -- /sbin/tini -- php "$@"
+elif [ "$1" = "phpcs" ]; then
+  set -- /sbin/tini -- php /composer/vendor/bin/"$@"
+elif [ "$1" = "/composer/vendor/bin/phpcbf" ]; then
+  set -- /sbin/tini -- php "$@"
+elif [ "$1" = "phpcbf" ]; then
+  set -- /sbin/tini -- php /composer/vendor/bin/"$@"
 fi
 
 exec "$@"
